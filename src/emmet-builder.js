7eb02db6af0d2cb9197a37007ea6297af1d22678
Degreet <degreetpro@gmail.com>
2020-12-11 16:44:29 +0200

    single tags

--- src/emmet-builder.js
+++ src/emmet-builder.js
@@ -1,9 +1,12 @@
+const singleTags = ["area", "base", "basefont", "bgsound",
+  "br", "col", "command", "embed", "hr", "img", "input",
+  "isindex", "keygen", "link", "meta", "param", "source",
+  "track", "wbr"]
+
 function emmetMake(query) {
   return document.createElement(query)
 }
 
 function emmetBuild(query) {
-  return `
-    <${query}></${query}>
-  `
+  return `<${query}>${singleTags.find(tag => tag == query) ? "" : `</${query}>`}`
 }
